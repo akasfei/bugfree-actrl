6b31f3c9a831ec9abcdbcca5d25c16a125756ce6
akasfei <akasfei@gmail.com>
2014-01-06 09:44:52 +0800

    Added db operations in Subject.js

--- models/Subject.js
+++ models/Subject.js
@@ -1,3 +1,5 @@
+var db = new require('../lib/Db.js')();
+
 function Subject(name, password, desc) {
   var hash = require('crypto').createHash('sha1');
   this.name = name;
@@ -7,8 +9,16 @@ function Subject(name, password, desc) {
 }
 
 Subject.prototype.auth = function(callback) {
-  // TODO: Connect the database, verify subject name and password, and retrieve subject desc
-  return callback(err);
+  db.find({name: this.name, password = this.password}, 'Subjects', {limit: 1}, function (err, docs) {
+    if (err)
+      return callback(err);
+    else if (docs.length < 1) {
+      return callback({err: 'AUTH_FAILED', msg: 'Error: Invalid username or password'});
+    } else {
+      this.desc = docs[0].desc;
+      return callback();
+    }
+  });
 };
 
 Subject.prototype.newObj = function(objname, desc, callback) {
@@ -39,104 +49,179 @@ Subject.prototype.newObj = function(objname, desc, callback) {
     cgrantors: ['root']
   };
 
-  // TODO: Save object to database
-
-  return callback(err);
+  db.insert(object, 'Objects', {}, function (err) {
+    return callback(err);
+  });
 };
 
 Subject.prototype.grant = function(subject, object, right, giveC, callback) {
   var self = this;
-  var obj = object; // TODO: Retrieve object info from database
 
-  if ( obj.access[right][self.name] 
-    && obj.access[right][self.name][cgrantors] 
-    && obj.access[right][self.name][cgrantors].length > 0) {
-    if (typeof obj.access[right][subject] === 'undefined')
-      // When the target subject doesn't have this right before
-      obj.access[right][subject] = {};
-
-    if ( typeof obj.access[right][subject][giveC ? "cgrantors": "grantors"] === 'undefined' 
-      || obj.access[right][subject][giveC ? "cgrantors": "grantors"].length < 1) {
-      // If no array is present then create a new one
-      obj.access[right][subject]["grantors"] = [subject];
-      if (giveC)
-        obj.access[right][subject]["cgrantors"] = [subject];
+  db.find({name: object}, 'Objects', {limit: 1}, function (err, docs) {
+    if (err)
+      return callback(err);
+    if (docs.length < 1)
+      return callback({err: 'OBJECT_NOT_FOUND', msg: 'Error: Object \'' + object + '\''});
+    var obj = docs[0];
+    if ( obj.access[right][self.name] 
+      && obj.access[right][self.name].cgrantors 
+      && obj.access[right][self.name].cgrantors.length > 0) {
+      if (typeof obj.access[right][subject] === 'undefined')
+        // When the target subject doesn't have this right before
+        obj.access[right][subject] = {};
+      else if (detectCircuit(obj, right, subject, self.name))
+        return callback({err: 'GRANT_CIRCUIT', msg: 'Error: You cannot grant right to your right\'s (in)direct grantor.'});
+      if ( typeof obj.access[right][subject].grantors === 'undefined' 
+        || obj.access[right][subject].grantors.length < 1) {
+        // If no array is present then create a new one
+        obj.access[right][subject].grantors = [subject];
+        if (giveC)
+          obj.access[right][subject].cgrantors = [subject];
+      } else {
+        if (obj.access[right][subject].grantors.indexOf(self.name))
+          return callback({err: 'SUBJECT_HAS_RIGHT', msg: 'Error: You have already granted subject right \'' + right + '\''});
+        obj.access[right][subject].grantors.push(subject);
+        if (giveC) {
+          if (typeof obj.access[right][subject].cgrantors !== 'undefined')
+            obj.access[right][subject].cgrantors.push(subject);
+          else
+            obj.access[right][subject].cgrantors = [subject];
+        }
+      }
     } else {
-      obj.access[right][subject]["grantors"].push(subject);
-      if (giveC)
-        obj.access[right][subject]["cgrantors"].push(subject);
+      return callback({err: 'SUBJECT_NOT_CTRLER', msg: 'Error: Current subject cannot control right \'' + right + '\''});
     }
-  } else {
-    return callback({{err: 'SUBJECT_NOT_CTRLER', msg: 'Error: Current subject cannot control right \'' + right + '\''});
-  }
-  // TODO: Save the modified object to database
-  return callback();
+    
+    db.update({name: obj.name}, obj, 'Objects', function (err) {
+      return callback(err);
+    });
+  });
 };
+
+var detectCircuit = function (obj, right, subject, grantor) {
+  for (var i = 0; i < obj.access[right][grantor].cgrantors.length; i++) {
+    var thisGrantor = obj.access[right][grantor].cgrantors[i];
+    if (thisGrantor == 'root')
+      return false;
+    if (obj.access[right][grantor].cgrantors.indexOf(subject) >= 0)
+      return true;
+    return detectCircuit(obj, right, subject, thisGrantor);
+  }
+}
 
 Subject.prototype.recind = function(subject, object, right, callback) {
   var self = this;
-  var obj = object; // TODO: Retrieve object info from database
+  db.find({name: object}, 'Objects', {limit: 1}, function (err, docs) {
+    if (err)
+      return callback(err);
+    if (docs.length < 1)
+      return callback({err: 'OBJECT_NOT_FOUND', msg: 'Error: Object \'' + object + '\''});
+    var obj = docs[0];
   
-  if (obj.access[right][self.name] && 
-      obj.access[right][self.name][cgrantors] && 
-      obj.access[right][self.name][cgrantors].length > 0) {
-    if ( typeof obj.access[right][subject] === 'undefined' 
-      || typeof obj.access[right][subject].grantors === 'undefined' 
-      || obj.access[right][subject].grantors.length < 1)
-        return callback({err: 'TARGET_NO_ACCESS', msg: 'Error: Target subject does not have right \'' + right + '\''});
-    else {
-      var index = obj.access[right][subject].grantors.indexOf(subject);
+    if (obj.access[right][self.name] && 
+        obj.access[right][self.name][cgrantors] && 
+        obj.access[right][self.name][cgrantors].length > 0) {
+      if ( typeof obj.access[right][subject] === 'undefined' 
+        || typeof obj.access[right][subject].grantors === 'undefined' 
+        || obj.access[right][subject].grantors.length < 1)
+          return callback({err: 'TARGET_NO_ACCESS', msg: 'Error: Target subject does not have right \'' + right + '\''});
+      else {
+        var index = obj.access[right][subject].grantors.indexOf(subject);
+        if (index >= 0) {
+          obj.access[right][subject].grantors.splice(index, 1);
+          if ( typeof obj.access[right][subject].cgrantors !== 'undefined' 
+            && (index = obj.access[right][subject].cgrantors.indexOf(subject)) >= 0) {
+              obj.access[right][subject].cgrantors.splice(index, 1);
+              if (obj.access[right][subject].cgrantors.length < 1)
+                recindAll(obj, right, subject);
+          }
+          if (obj.access[right][subject].grantors.length < 1)
+            delete obj.access[right][subject];
+        } else
+          return callback({err: 'SUBJECT_NOT_GRANTED', msg: 'Error: Target subject does not have right \'' + right + '\''}); 
+      }
+    } else {
+      return callback({err: 'SUBJECT_NOT_CTRLER', msg: 'Error: Current subject cannot control right \'' + right + '\''});
+    }
+    
+    db.update({name: obj.name}, obj, 'Objects', function (err) {
+      return callback(err);
+    });
+  });
+};
+
+var recindAll = function (obj, right, grantor) {
+  for (var subject in obj.access[right]) {
+    var index;
+    if (typeof obj.access[right][subject].grantors !== 'undefined') {
+      index = obc.access[right][subject].grantors.indexOf(grantor);
       if (index >= 0) {
         obj.access[right][subject].grantors.splice(index, 1);
         if ( typeof obj.access[right][subject].cgrantors !== 'undefined' 
-          && (index = obj.access[right][subject].cgrantors.indexOf(subject)) >= 0) {
+        && ( index = obj.access[right][subject].cgrantors.indexOf(grantor)) >= 0) {
             obj.access[right][subject].cgrantors.splice(index, 1);
             if (obj.access[right][subject].cgrantors.length < 1)
-              // TODO: Remove other subjects granted by this subject
+              recindAll(obj, right, subject);
         }
         if (obj.access[right][subject].grantors.length < 1)
           delete obj.access[right][subject];
-      } else
-        return callback({err: 'SUBJECT_NOT_GRANTED', msg: 'Error: Target subject does not have right \'' + right + '\''}); 
+      }
+    } else {
+      delete obj.access[right][subject];
     }
-  } else {
-    return callback({err: 'SUBJECT_NOT_CTRLER', msg: 'Error: Current subject cannot control right \'' + right + '\''});
   }
-  // TODO: Save the modified object to database
-  return callback();
-};
+}
 
 Subject.prototype.remove = function(object, callback) {
   var self = this;
-  var obj = object; // TODO: Retrieve object info from database
+  db.find({name: object}, 'Objects', {limit: 1}, function (err, docs) {
+    if (err)
+      return callback(err);
+    if (docs.length < 1)
+      return callback({err: 'OBJECT_NOT_FOUND', msg: 'Error: Object \'' + object + '\''});
+    var obj = docs[0];
 
-  if (typeof object.access['c'][self.name] !== 'undefined') {
-    // TODO: Remove the object
-    return callback();
-  } else
-    return callback({err: 'SUBJECT_NOT_AUTHORIZED', msg: 'Error: Current subject is not controler of object \'' + object + '\''});
+    if (typeof obj.access['c'][self.name] !== 'undefined') {
+      db.remove({name: object}, {}, function (err) {
+        return callback(err);
+      });
+    } else
+      return callback({err: 'SUBJECT_NOT_AUTHORIZED', msg: 'Error: Current subject is not controler of object \'' + object + '\''});
+  });
 };
 
 Subject.prototype.read = function(object, callback) {
   var self = this;
-  var obj = object; // TODO: Retrieve object info from database
+  db.find({name: object}, 'Objects', {limit: 1}, function (err, docs) {
+    if (err)
+      return callback(err);
+    if (docs.length < 1)
+      return callback({err: 'OBJECT_NOT_FOUND', msg: 'Error: Object \'' + object + '\''});
+    var obj = docs[0];
 
-  if (typeof object.access['r'][self.name] !== 'undefined') {
-    // TODO: Retrieve the object
-    return callback(null, data);
-  } else
-    return callback({err: 'SUBJECT_NOT_AUTHORIZED', msg: 'Error: Current subject cannot read object \'' + object + '\''});
+    if (typeof obj.access['r'][self.name] !== 'undefined') {
+      return callback(null, obj);
+    } else
+      return callback({err: 'SUBJECT_NOT_AUTHORIZED', msg: 'Error: Current subject cannot read object \'' + object + '\''});
+  });
 };
 
 Subject.prototype.write = function(object, newDesc, callback) {
   var self = this;
-  var obj = object; // TODO: Retrieve object info from database
+  db.find({name: object}, 'Objects', {limit: 1}, function (err, docs) {
+    if (err)
+      return callback(err);
+    if (docs.length < 1)
+      return callback({err: 'OBJECT_NOT_FOUND', msg: 'Error: Object \'' + object + '\''});
+    var obj = docs[0];
 
-  if (typeof object.access['r'][self.name] !== 'undefined') {
-    // TODO: Update the object
-    return callback();
-  } else
-    return callback({err: 'SUBJECT_NOT_AUTHORIZED', msg: 'Error: Current subject cannot read object \'' + object + '\''});
+    if (typeof object.access['r'][self.name] !== 'undefined') {
+      db.update({name: object}, {$set: {desc: newDesc}}, 'Objects', function (err) { // TODO: look up mongodb docs and verify
+        return callback(err);
+      });
+    } else
+      return callback({err: 'SUBJECT_NOT_AUTHORIZED', msg: 'Error: Current subject cannot read object \'' + object + '\''});
+  });
 };
 
 module.exports = Subject;
