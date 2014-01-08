48cba51080685d5546e27d37af515baafb2a1d58
akasfei <akasfei@gmail.com>
2014-01-08 09:57:47 +0800

    Implemented object removing and editing

--- html/js/app-list.js
+++ html/js/app-list.js
@@ -37,6 +37,77 @@
       });
     });
 
+    $('.object-list').on('click', '.obj-remove', function (e) {
+      var self = $(this);
+      var name = self.parents('tr').attr('data-name');
+      if ( !confirm('Comfirm removal of object "' + name + '"?') )
+        return;
+      $.ajax({
+        url: '/objects/remove',
+        method: 'GET',
+        data: {n: name},
+        dataType: 'json',
+        success: function (data, status, xhr) {
+          if (data && data.err) {
+            $('.float-msg').msg({msg: data.err + data.msg ? '\n' + data.msg: ''});
+            return;
+          }
+          $('.object-list-refresh').trigger('click');
+        }
+      });
+    });
+
+    $('.object-list').on('click', '.obj-edit', function (e) {
+      var self = $(this);
+      var name = self.parents('tr').attr('data-name');
+      $('#obj_edit_name').text(name);
+      $('#obj_edit_desc').val('');
+      $('#obj-edit-modal').modal('show');
+    });
+
+    $('#obj-edit-getdesc').on('click', function (e) {
+      var self = $(this);
+      $.ajax({
+        url: '/object',
+        method: 'GET',
+        data: {n: $('#obj_edit_name').text()},
+        dataType: 'json',
+        success: function (data, status, xhr) {
+          if (data.err) {
+            $('.float-msg').msg({msg: data.err + data.msg ? '\n' + data.msg: ''});
+            return;
+          }
+          $('#obj_edit_desc').val(data.desc);
+        }
+      });
+    });
+
+    $('#obj_edit_submit').on('click', function (e) {
+      var self = $(this);
+      var updatedata = {
+        obj_name: $('#obj_edit_name').text(),
+        obj_new_desc: $('#obj_edit_desc').val()
+      }
+      if (updatedata.obj_name.length < 1 || updatedata.obj_new_desc.length < 1) {
+        $('.float-msg').msg({msg: 'Object name or description invalid.'});
+        return;
+      }
+      $.ajax({
+        url: '/objects/write',
+        method: 'POST',
+        data: updatedata,
+        dataType: 'json',
+        success: function (data, status, xhr) {
+          if (data && data.err) {
+            $('.float-msg').msg({msg: data.err + data.msg ? '\n' + data.msg: ''});
+            return;
+          }
+          $('#obj-edit-modal').modal('hide');
+          $('.object-list-refresh').trigger('click');
+        }
+      });
+    });
+
     $('#new_obj_submit').on('click', function (e) {
       var obj = {
         name: $('#new_obj_name').val(),
